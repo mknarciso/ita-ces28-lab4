f340660af99af0e30784f515a56dd1140e92faf9
rafaelvidal1 <rafael.vidal125@gmail.com>
2016-11-08 14:17:08 -0200

    psTest, ServiceNode

---
+++ src/testes/psTest.java
@@ -0,0 +1,22 @@
+package testes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import ps.PS;
+import ps.ProdutoNode;
+
+public class psTest {
+
+	@Test
+	public void test() {
+		PS produtoT1 = new ProdutoNode("Teste1", 15, "Frutas", 1 , "aaa");
+		
+		
+		
+		
+		
+	}
+
+}
