c398b44b46bc1cd8790162c72ce98ff9d205a85f
Murilo K Narciso <mknarciso@hotmail.com>
2016-11-08 14:09:35 -0200

    addPS

--- src/ps/DbPS.java
+++ src/ps/DbPS.java
@@ -1,9 +1,16 @@
 package ps;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DbPS {
+	List<PS> dados = new ArrayList<PS>();
 	List<PS> getAll(){
 		return null;
+	}
+
+	public void addPS(PS newProduct) {
+		dados.add(newProduct);
+		
 	}
 }
